3c0330bc0f767fcf96035bb55f5678556464e5aa
NB <n.baumgaertner@hotmail.com>
2023-07-14 00:17:45 +0200

    ups

--- LP.Plotter/Data/DataService.cs
+++ LP.Plotter/Data/DataService.cs
@@ -14,6 +14,21 @@ public class DataService
 
     public async Task<List<DataObject>> GetFileInfos()
     {
+        var response = await httpClient.GetAsync("csvdata/files_index.txt");
+        response.EnsureSuccessStatusCode();
+        var text = await response.Content.ReadAsStringAsync();
+        return text.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)
+        .Select(x => new DataObject()
+        {
+            FileName = Path.GetFileName(x),
+            Path = x,
+            Url = Path.Combine("csvdata", x),
+            Channels = null,
+        }).ToList();
+    }
+
+    private async Task<List<DataObject>> GetFileInfosFromGithub()
+    {
         string url = "https://api.github.com/repos/NicholasBaum/TyrePlot/git/trees/gh-pages?recursive=1";
         HttpResponseMessage response = await httpClient.GetAsync(url);
         response.EnsureSuccessStatusCode();
