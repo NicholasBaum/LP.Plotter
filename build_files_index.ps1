4650c344bb0a635bbf3028f2d293d8e00ed0ccb2
NB <n.baumgaertner@hotmail.com>
2023-07-13 15:01:30 +0200

    updated db creation

--- build_files_index.ps1
+++ build_files_index.ps1
@@ -6,8 +6,11 @@ param (
 # Get the full path of the folder
 $FolderPath = Convert-Path -Path $FolderPath
 
-# Get all files within the folder and its subfolders
-$Files = Get-ChildItem -Path $FolderPath -File -Recurse
+# Create full path of output file
+$OutputFilePath = Join-Path -Path $FolderPath -ChildPath "files_index.txt"
+
+# Get all files within the folder and its subfolders and filter the outputfile itself if it already exists
+$Files = Get-ChildItem -Path $FolderPath -File -Recurse | Where-Object { $_.FullName -ne $OutputFilePath }
 
 # Create an array to store the relative paths
 $RelativePaths = @()
@@ -19,7 +22,6 @@ foreach ($File in $Files) {
 }
 
 # Write the relative paths to a file
-$OutputFilePath = Join-Path -Path $FolderPath -ChildPath "RelativePaths.txt"
 $RelativePaths | Out-File -FilePath $OutputFilePath
 
 # Output the file path where the relative paths are stored
