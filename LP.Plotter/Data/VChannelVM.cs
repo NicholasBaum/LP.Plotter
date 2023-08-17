9e25d0720868595556997da00ebb337590252ba8
NB <n.baumgaertner@hotmail.com>
2023-08-18 01:28:14 +0200

    better tracker label

--- LP.Plotter/Data/VChannelVM.cs
+++ LP.Plotter/Data/VChannelVM.cs
@@ -9,15 +9,22 @@ public class VChannelVM : LineSeries
     public int Id { get; } = lastId++;
     public string Name { get; set; }
     public bool Selected { get; set; }
-    
+
     public VChannelVM(string name)
     {
         Name = name;
+        Title = name;
+        TrackerFormatString = "{0}\n{1}: {2:0.00}\n{3}: {4:0.00}";
     }
 
-    public VChannelVM(string name, IEnumerable<DataPoint> points)
+    public VChannelVM(string name, IEnumerable<DataPoint> points) : this(name)
     {
-        Name = name;
         this.Points.AddRange(points);
     }
 }
+
+//{0} the title of the series
+//{1} the title of the x-axis
+//{2} the x-value
+//{3} the title of the y-axis
+//{4} the y-value
