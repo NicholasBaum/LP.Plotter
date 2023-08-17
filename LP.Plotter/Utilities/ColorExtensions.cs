7df9357612b10b1092bae01547affd98df02084d
NB <n.baumgaertner@hotmail.com>
2023-08-17 02:40:28 +0200

    better icons, totally worth the time

--- LP.Plotter/Utilities/ColorExtensions.cs
+++ LP.Plotter/Utilities/ColorExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using MudBlazor.Utilities;
 using OxyPlot;
+using System.IO;
 
 namespace LP.Plotter.Utilities;
 
@@ -41,4 +42,11 @@ public static class ColorExtensions
     {
         return DefaultColors[currentColorIndex++ % DefaultColors.Count];
     }
+
+}
+
+public class CustomIcons
+{
+    public const string ZoomInHorizontal = @"<g><g><rect fill=""none"" height=""24"" width=""24"" /></g><g><g><g><path d=""M 3,9 5.3,6.7 8.17,9.59 9.59,8.17 6.7,5.3 9,3 H 3 Z"" transform=""translate(14,0) rotate(-45, 12, 12)""/><path d=""M 3,9 5.3,6.7 8.17,9.59 9.59,8.17 6.7,5.3 9,3 H 3 Z"" transform=""translate(-14,0) rotate(135, 12, 12)"" /></g></g></g></g>";
+    public const string ZoomOutHorizontal = @"<g><g><rect fill=""none"" height=""24"" width=""24"" /></g><g><g><g><path d=""M 3,9 5.3,6.7 8.17,9.59 9.59,8.17 6.7,5.3 9,3 H 3 Z"" transform=""translate(3,0) rotate(-45, 12, 12)""/><path d=""M 3,9 5.3,6.7 8.17,9.59 9.59,8.17 6.7,5.3 9,3 H 3 Z"" transform=""translate(-3,0) rotate(135, 12, 12)"" /></g></g></g></g>";
 }
