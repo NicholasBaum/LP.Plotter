4aa371be664fb0f05bda7ffb166315bd633c3c2d
NB <n.baumgaertner@hotmail.com>
2023-08-19 19:14:37 +0200

    added wpf stuff

---
+++ LP.Plotter.Core/Services/LocalDataService.cs
@@ -0,0 +1,24 @@
+﻿
+using LP.Plotter.Core.Models;
+
+namespace LP.Plotter.Core.Services;
+
+public class LocalDataService
+{
+    public VChannelSet LoadTest()
+    {
+        var folder = @"D:\work\LP.Plotter\LP.Plotter\wwwroot\csvdata\events\imola_2023\T2303_IMO_#29\D1PMRun1";
+        var files = Directory.GetFiles(folder);
+        var set = files.Select(x =>
+        {
+            var text = File.ReadAllText(x);
+            return VChannelSet.Create(new CsvInfo()
+            {
+                FileName = Path.GetFileName(x),
+                Path = x,
+                Url = x
+            }, text);
+        }).ToList();
+        return VChannelSet.CreateMerged(set);
+    }
+}
---
+++ LP.Plotter.WpfSkia.Test/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using LP.Plotter.Core.Models;
+using LP.Plotter.Core.Services;
+using OxyPlot;
+using OxyPlot.SkiaSharp.Wpf;
+using System.Windows;
+
+namespace LP.Plotter.WpfSkia.Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            Loaded += MainWindow_Initialized;
+        }
+
+        private void MainWindow_Initialized(object? sender, EventArgs e)
+        {
+            Init();
+        }
+
+        public PlotModel OxyModel { get; set; } = new PlotModel();
+        public ChannelPlotModel Model { get; set; } = new ChannelPlotModel();
+        private LocalDataService dataService = new LocalDataService();
+        private void Init()
+        {
+            var data = dataService.LoadTest();
+            Model.Add(data);
+            Model.Draw(OxyModel);
+            OxyModel.InvalidatePlot(true);
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            OxyModel.ResetAllAxes();
+            OxyModel.InvalidatePlot(true);
+        }
+    }
+}
