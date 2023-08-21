96105aba7709f211e81608fda464efdf9ead4ecf
NB <n.baumgaertner@hotmail.com>
2023-08-21 03:30:43 +0200

    added stuff

---
+++ LP.Plot.Test/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using SkiaSharp.Views.Desktop;
+using SkiaSharp;
+using System.Windows;
+using System.Windows.Input;
+using System.Diagnostics;
+using LP.Plotter.Core.Drawing;
+using LP.Plotter.Core.Services;
+
+namespace LP.Plot.Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+
+            MouseDown += OnMouseDown;
+            MouseMove += OnMouseMove;
+            MouseUp += OnMouseUp;
+            var data = new LocalDataService().LoadTest();
+            drawer = new SignalPlotter(data);
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private SignalPlotter drawer;
+
+        private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            if (!sw.IsRunning)
+                sw.Start();
+            this.drawer.Draw(e.Surface.Canvas, e.Info);
+            this.DrawInfo(e.Surface.Canvas, e.Info);
+        }
+
+        private void DrawInfo(SKCanvas canvas, SKImageInfo imageInfo)
+        {
+            using var black = new SKPaint
+            {
+                Color = SKColors.Black,
+                IsAntialias = true,
+                Style = SKPaintStyle.Fill,
+                TextAlign = SKTextAlign.Left,
+            };
+            using var white = new SKPaint
+            {
+                Color = SKColors.White,
+                IsAntialias = true,
+                Style = SKPaintStyle.Fill,
+                TextAlign = SKTextAlign.Left,
+                TextSize = 20,
+            };
+            using var font = new SKFont
+            {
+                Size = 24
+            };
+
+            var coord = new SKPoint(imageInfo.Width + 200, imageInfo.Height + 200);
+            canvas.DrawRect(0, 0, 100, 100, black);
+
+            var text = $"Frames {frameCount++}";
+            SKRect bounds = new SKRect();
+            white.MeasureText(text, ref bounds);
+            canvas.DrawText(text, 0, 1.5f * bounds.Height, font, white);
+            text = $"Fps {(frameCount / (double)sw.Elapsed.TotalSeconds):0.00}";
+
+            white.MeasureText(text, ref bounds);
+            canvas.DrawText(text, 0, bounds.Height * 2.5f, font, white);
+        }
+
+        private int frameCount = 0;
+        private Stopwatch sw = new Stopwatch();
+
+
+        private Point lastMousePos;
+
+        private void OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            frameCount = 0;
+            sw.Restart();
+            lastMousePos = e.GetPosition(this);
+            CaptureMouse(); // Capture mouse events to track movement outside the control
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (IsMouseCaptured)
+            {
+                Point newMousePos = e.GetPosition(this);
+                double deltaX = newMousePos.X - lastMousePos.X;
+                double deltaY = newMousePos.Y - lastMousePos.Y;
+
+                // Update the content's position (e.g., Canvas' Left and Top properties)
+                // based on deltaX and deltaY
+
+                lastMousePos = newMousePos;
+                skiaEl.InvalidateVisual();
+
+            }
+        }
+
+        private void OnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (IsMouseCaptured)
+                ReleaseMouseCapture();
+            var diff = lastMousePos - e.GetPosition(this);
+        }
+    }
+}
---
+++ LP.Plotter.Core/Drawing/SignalPlotter.cs
@@ -0,0 +1,115 @@
+﻿using LP.Plotter.Core.Models;
+using SkiaSharp;
+
+namespace LP.Plotter.Core.Drawing;
+
+public class SignalPlotter
+{
+    private readonly VChannelSet data;
+    private List<(float[] yvalues, Axis YAxis, SKPaint Paint)> channels;
+    private Axis XAxis;
+
+    public SignalPlotter(VChannelSet data)
+    {
+        this.data = data;
+
+        paints.Add(paint1);
+        paints.Add(paint2);
+        paints.Add(paint3);
+        paints.Add(paint4);
+        paints.Add(paint5);
+
+        this.channels = new();
+        foreach (var c in data.Channels.Where(x => x.Name.Contains("Speed") || x.Name.Contains("TT")))
+        {
+            var yValues = c.Points.Select(x => (float)x.Y).ToArray();
+            var yaxis = new Axis() { Min = (float)yValues.Min(), Max = (float)yValues.Max() }.Scale(1.1f);
+            this.channels.Add((yValues, yaxis, NextPaint()));
+        }
+        var min = data.SpeedChannel.Points.First().X;
+        var max = data.SpeedChannel.Points.Last().X;
+        this.XAxis = new Axis() { Min = (float)min, Max = (float)max };
+    }
+
+    public void Draw(SKCanvas canvas, SKImageInfo imageInfo)
+    {
+        canvas.Clear(SKColors.Black);
+
+        foreach (var channel in this.channels)
+        {
+            DrawSignal(channel.yvalues, channel.YAxis, channel.Paint, canvas, imageInfo);
+        }
+    }
+
+    private void DrawSignal(float[] channel, Axis yAxis, SKPaint paint, SKCanvas canvas, SKImageInfo imageInfo)
+    {
+        var m = imageInfo.Height / (yAxis.Min - yAxis.Max);
+        var dp = imageInfo.Width / (float)(channel.Length - 1);
+        var lastx = 0.0f;
+
+        var lasty = (channel[0] - yAxis.Max) * m;
+
+        for (int i = 0; i < channel.Length - 1; i++)
+        {
+            var newX = i * dp;
+            var newy = (channel[i] - yAxis.Max) * m;
+            canvas.DrawLine(lastx, lasty, newX, newy, paint);
+            lastx = newX;
+            lasty = newy;
+        }
+    }
+
+    private class Axis
+    {
+        public float Min { get; set; }
+        public float Max { get; set; }
+        public float Length => Max - Min;
+        public Axis Scale(float s) => new Axis() { Min = Min - (s - 1) * Length, Max = Max + (s - 1) * Length };
+    }
+
+    int currentPaintIndex = 0;
+    List<SKPaint> paints = new List<SKPaint>();
+    private SKPaint NextPaint() => paints[currentPaintIndex++ % paints.Count];
+
+    private SKPaint paint1 = new SKPaint()
+    {
+        Color = SKColors.Orange,
+        StrokeWidth = 3,
+        IsAntialias = true,
+        Style = SKPaintStyle.Stroke
+    };
+
+    private SKPaint paint2 = new SKPaint()
+    {
+        Color = SKColors.Blue,
+        StrokeWidth = 3,
+        IsAntialias = true,
+        Style = SKPaintStyle.Stroke
+    };
+
+    private SKPaint paint3 = new SKPaint()
+    {
+        Color = SKColors.Red,
+        StrokeWidth = 3,
+        IsAntialias = true,
+        Style = SKPaintStyle.Stroke
+    };
+
+    private SKPaint paint4 = new SKPaint()
+    {
+        Color = SKColors.Green,
+        StrokeWidth = 3,
+        IsAntialias = true,
+        Style = SKPaintStyle.Stroke
+    };
+
+    private SKPaint paint5 = new SKPaint()
+    {
+        Color = SKColors.Orange,
+        StrokeWidth = 3,
+        IsAntialias = true,
+        Style = SKPaintStyle.Stroke
+    };
+
+}
+
