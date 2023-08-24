da4d30003d48a9924634a37a700d6db02e67dda5
NB <n.baumgaertner@hotmail.com>
2023-08-24 03:51:43 +0200

    ups

--- LP.Plot.Core/Signal/SignalRenderer.cs
+++ LP.Plot.Core/Signal/SignalRenderer.cs
@@ -2,7 +2,6 @@
 using LP.Plot.Skia;
 using LP.Plotter.Core;
 using SkiaSharp;
-using System.Runtime.Intrinsics.X86;
 
 namespace LP.Plot.Core.Signal;
 
@@ -26,7 +25,8 @@ public class SignalRenderer : IRenderable
     {
         ctx.Canvas.Clear(SKColors.Black);
 
-        CreateDecimatedPath(data.YValues, data.XRange, XAxis, YAxis, ctx.Width, ctx.Height);
+        CreateDecimatedPath2(data.YValues, data.XRange, XAxis, YAxis, ctx.Width, ctx.Height);
+        //CreatePath(data.YValues, data.XRange, XAxis, YAxis, ctx.Width, ctx.Height);
         ctx.Canvas.DrawPath(path, Paint);
         //RenderHighDensity(data.YValues, data.XRange, YAxis, ctx.Canvas, Paint, ctx.Width, ctx.Height);
 
@@ -75,13 +75,76 @@ public class SignalRenderer : IRenderable
         return bars;
     }
 
+    // tranlates pixels to coordinate space and cycles through the samples taking 4 samples per pixel
+    private void CreateDecimatedPath2(double[] yValues, Span xDataRange, Axis xAxis, Axis yAxis, int imageWidth, int imageHeight)
+    {
+        SKSpaceTransform t = new(imageWidth, imageHeight, xAxis.Range, yAxis.Range);
+        path.Reset();
+        var dx = xDataRange.Length / (yValues.Length - 1); // units per sample
+        var x = xDataRange.Min;
+        // the method uses the start, min, max and end y-value in a pixel
+        var start = new Point(0, yValues[0]);
+        var min = start;
+        var max = start;
+        var dpx = XAxis.Length / imageWidth; // units per pixel
+        var nextPixelLeftBoundary = xDataRange.Min + dpx;
+        path.MoveTo(t.ToPixelSpaceX(xDataRange.Min), t.ToPixelSpaceY(yValues[0]));
+        var reset = false;
+        for (var i = 1; i < yValues.Length; i++)
+        {
+            x += dx;
+            if (reset)
+            {
+                start = new Point(x, yValues[i]);
+                min = start;
+                max = start;
+                nextPixelLeftBoundary += dpx;
+                reset = false;
+            }
+            else if (x + dx > nextPixelLeftBoundary)
+            {
+                path.LineTo(t.ToPixelSpaceX(start.X), t.ToPixelSpaceY(start.Y));
+                path.LineTo(t.ToPixelSpaceX(min.X), t.ToPixelSpaceY(min.Y));
+                path.LineTo(t.ToPixelSpaceX(max.X), t.ToPixelSpaceY(max.Y));
+                path.LineTo(t.ToPixelSpaceX(x), t.ToPixelSpaceY(yValues[i]));
+                //var p = t.ToPixelSpaceX(nextPixelLeftBoundary);
+                //path.LineTo(p - 1, t.ToPixelSpaceY(start.Y));
+                //path.LineTo(p - 0.75f, t.ToPixelSpaceY(min.Y));
+                //path.LineTo(p - 0.5f, t.ToPixelSpaceY(max.Y));
+                //path.LineTo(p - 0.25f, t.ToPixelSpaceY(yValues[i]));
+                reset = true;
+            }
+            else
+            {
+                if (min.Y > yValues[i])
+                    min = new Point(x, yValues[i]);
+                if (max.Y < yValues[i])
+                    max = new Point(x, yValues[i]);
+            }
+        }
+    }
 
+    private ref struct Point
+    {
+        public readonly double X;
+        public readonly double Y;
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    // calculates what samples lie in a pixel only works if this can be calculated easily
+    // code is only prototype 
+    // i think it creates step lines
     private void CreateDecimatedPath(double[] yValues, Span xDataRange, Axis xAxis, Axis yAxis, int imageWidth, int imageHeight)
     {
         SKSpaceTransform t = new(imageWidth, imageHeight, xAxis.Range, yAxis.Range);
         path.Reset();
         var unitsPerPixel = XAxis.Length / imageWidth;
-        var unitsPerIndex = xDataRange.Length / (yValues.Length - 1);
+        var dx = xDataRange.Length / (yValues.Length - 1);
         var offset = (xDataRange.Min - XAxis.Min) / unitsPerPixel;
         var indexOffset = (int)((xDataRange.Min - XAxis.Min) / unitsPerPixel);
         path.MoveTo(t.ToPixelSpaceX(xDataRange.Min), t.ToPixelSpaceY(yValues[0]));
@@ -89,8 +152,8 @@ public class SignalRenderer : IRenderable
         {
             var xLeft = i * unitsPerPixel;
             var xRight = xLeft + unitsPerPixel;
-            var leftIndex = (int)(xLeft / unitsPerIndex);
-            var rightIndex = (int)(xRight / unitsPerIndex) + 1;
+            var leftIndex = (int)(xLeft / dx);
+            var rightIndex = (int)(xRight / dx) + 1;
             var min = float.MaxValue;
             var max = float.MinValue;
             if (rightIndex >= yValues.Length)
@@ -120,7 +183,7 @@ public class SignalRenderer : IRenderable
     {
         SKSpaceTransform t = new(imageWidth, imageHeight, xAxis.Range, yAxis.Range);
         path.Reset();
-        var dx = data.Period;
+        var dx = xDataRange.Length / (yValues.Length - 1);
         path.MoveTo(t.ToPixelSpaceX(xDataRange.Min), t.ToPixelSpaceY(yValues[0]));
         for (int i = 1; i < yValues.Length - 1; i++)
             path.LineTo(t.ToPixelSpaceX(xDataRange.Min + i * dx), t.ToPixelSpaceY(yValues[i]));
