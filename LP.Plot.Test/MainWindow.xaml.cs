4aac89e77a0352474eded2f8545a5809aa1ff5ce
NB <n.baumgaertner@hotmail.com>
2023-08-26 21:39:09 +0200

    refactor

--- LP.Plot.Test/MainWindow.xaml.cs
+++ LP.Plot.Test/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using SkiaSharp.Views.Desktop;
 using System.Windows;
-using System.Windows.Input;
 using System.Diagnostics;
 using LP.Plotter.Core.Services;
 using LP.Plot.Core;
@@ -13,70 +12,13 @@ namespace LP.Plot.Test
     public partial class MainWindow : Window
     {
         private Core.Plot signal;
-        private RenderInfo renderInfo = new();
-        private Stopwatch frameTimer = new Stopwatch();
-
 
         public MainWindow()
         {
             InitializeComponent();
 
-            MouseDown += OnMouseDown;
-            MouseMove += OnMouseMove;
-            MouseUp += OnMouseUp;
-            MouseWheel += OnMouseWheel;
-
             var data = new LocalDataService().LoadSignal_M();
-            this.signal = Core.Plot.CreateSignal(data);
-        }
-
-        private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
-        {
-            frameTimer.Restart();
-            this.signal.Render(new SkiaRenderContext(e.Surface.Canvas, e.Info.Width, e.Info.Height));
-            renderInfo.PaintRenderInfo(e.Surface.Canvas, e.Info);
-            Debug.WriteLine($"Rendertime {frameTimer.Elapsed.TotalSeconds}:0.00");
-        }
-
-        private Point lastMousePos;
-
-        private void OnMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            renderInfo.Restart();
-            lastMousePos = e.GetPosition(this);
-            CaptureMouse(); // Capture mouse events to track movement outside the control
-        }
-
-        private void OnMouseMove(object sender, MouseEventArgs e)
-        {
-            if (IsMouseCaptured)
-            {
-                Point newMousePos = e.GetPosition(this);
-                double deltaX = newMousePos.X - lastMousePos.X;
-                double deltaY = newMousePos.Y - lastMousePos.Y;
-                var panx = -deltaX / skiaEl.ActualWidth;
-                var pany = deltaY / skiaEl.ActualHeight;
-                signal.Pan(panx, pany);
-                skiaEl.InvalidateVisual();
-                lastMousePos = newMousePos;
-            }
-        }
-
-        private void OnMouseUp(object sender, MouseButtonEventArgs e)
-        {
-            if (IsMouseCaptured)
-                ReleaseMouseCapture();
-            var diff = lastMousePos - e.GetPosition(this);
-        }
-
-        private void OnMouseWheel(object sender, MouseWheelEventArgs e)
-        {
-            var factor = Math.Sign(e.Delta) > 0 ? 0.9 : 1.1;
-            var pos = e.GetPosition(skiaEl);
-            var xPos = pos.X / skiaEl.ActualWidth;
-            var yPos = 1 - pos.Y / skiaEl.ActualHeight;
-            signal.ZoomAt(factor, xPos, yPos);
-            skiaEl.InvalidateVisual();
+            this.signal = WpfPlot.CreateSignal(data, this, skiaEl);
         }
     }
 }
---
+++ LP.Plot.Test/WpfPlot.cs
@@ -0,0 +1,82 @@
+﻿using LP.Plot.Core;
+using LP.Plot.Core.Signal;
+using SkiaSharp.Views.Desktop;
+using SkiaSharp.Views.WPF;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace LP.Plot.Test;
+
+public class WpfPlot : Core.Plot
+{
+    private RenderInfo renderInfo = new();
+    private Stopwatch frameTimer = new Stopwatch();
+    private Point lastMousePos;
+    private Control control;
+    private SKElement skiaEl;
+
+    public static Core.Plot CreateSignal(ISignalSource data, Control control, SKElement skiaEl)
+    {
+        var plot = new WpfPlot();
+        plot.AddSignal(data);
+
+        skiaEl.PaintSurface += plot.OnPaintSurface;
+
+        control.MouseDown += plot.OnMouseDown;
+        control.MouseMove += plot.OnMouseMove;
+        control.MouseUp += plot.OnMouseUp;
+        control.MouseWheel += plot.OnMouseWheel;
+        plot.control = control;
+        plot.skiaEl = skiaEl;
+        return plot;
+    }
+
+    private void OnPaintSurface(object? sender, SKPaintSurfaceEventArgs e)
+    {
+        frameTimer.Restart();
+        Render(new SkiaRenderContext(e.Surface.Canvas, e.Info.Width, e.Info.Height));
+        renderInfo.PaintRenderInfo(e.Surface.Canvas, e.Info);
+        Debug.WriteLine($"Rendertime {frameTimer.Elapsed.TotalSeconds}:0.00");
+    }
+
+    private void OnMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        renderInfo.Restart();
+        lastMousePos = e.GetPosition(control);
+        control.CaptureMouse(); // Capture mouse events to track movement outside the control
+    }
+
+    private void OnMouseMove(object sender, MouseEventArgs e)
+    {
+        if (control.IsMouseCaptured)
+        {
+            Point newMousePos = e.GetPosition(control);
+            double deltaX = newMousePos.X - lastMousePos.X;
+            double deltaY = newMousePos.Y - lastMousePos.Y;
+            var panx = -deltaX / skiaEl.ActualWidth;
+            var pany = deltaY / skiaEl.ActualHeight;
+            Pan(panx, pany);
+            skiaEl.InvalidateVisual();
+            lastMousePos = newMousePos;
+        }
+    }
+
+    private void OnMouseUp(object sender, MouseButtonEventArgs e)
+    {
+        if (control.IsMouseCaptured)
+            control.ReleaseMouseCapture();
+        var diff = lastMousePos - e.GetPosition(control);
+    }
+
+    private void OnMouseWheel(object sender, MouseWheelEventArgs e)
+    {
+        var factor = Math.Sign(e.Delta) > 0 ? 0.9 : 1.1;
+        var pos = e.GetPosition(skiaEl);
+        var xPos = pos.X / skiaEl.ActualWidth;
+        var yPos = 1 - pos.Y / skiaEl.ActualHeight;
+        this.ZoomAt(factor, xPos, yPos);
+        skiaEl.InvalidateVisual();
+    }
+}
