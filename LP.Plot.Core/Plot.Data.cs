bb095f62c02918719f7caff6236b6da0419caff6
NB <n.baumgaertner@hotmail.com>
2023-09-01 01:57:36 +0200

    moved to LP.Plot

---
+++ LP.Plot.Core/Plot.Data.cs
@@ -0,0 +1,32 @@
+﻿using SkiaSharp;
+
+namespace LP.Plot.Core;
+
+public interface IData
+{
+    public List<SignalSet> Sets { get; }
+    public void Remove(SignalSet set);
+}
+
+public class SignalVM
+{
+    public string Name { get; set; } = "";
+    public bool Selected { get; set; }
+    public Axis YAxis { get; set; }
+    public bool IsVisible { get; set; }
+    public SKColor Color { get; set; }
+}
+
+public class SignalSet
+{
+    public string Name { get; set; } = "";
+    public List<SignalVM> Channels { get; set; } = new();
+    public List<SignalVM> SourceChannels { get; set; } = new();
+}
+
+public partial class Plot : IData
+{
+    public List<SignalSet> Sets { get; set; } = new();
+
+    public void Remove(SignalSet set) => Sets.Remove(set);
+}
--- LP.Plotter/Utilities/ColorExtensions.cs
+++ LP.Plotter/Utilities/ColorExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using MudBlazor.Utilities;
 using OxyPlot;
+using SkiaSharp;
 
 namespace LP.Plotter.Utilities;
 
@@ -7,4 +8,6 @@ public static class ColorExtensions
 {
     public static OxyColor ToOxyColor(this MudColor color) => OxyColor.FromArgb(color.A, color.R, color.G, color.B);
     public static MudColor ToMudColor(this OxyColor color) => new MudColor(color.R, color.G, color.B, color.A);
+    public static SKColor ToSKColor(this MudColor color) => new SKColor(color.A, color.R, color.G, color.B);
+    public static MudColor ToMudColor(this SKColor color) => new MudColor(color.Red, color.Green, color.Blue, color.Alpha);
 }
