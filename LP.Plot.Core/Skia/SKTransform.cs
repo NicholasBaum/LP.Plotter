6f5d6f79eed6bfd534b1d830c4372460f2df1c7f
NB <n.baumgaertner@hotmail.com>
2023-09-07 14:17:02 +0200

    removed LinearTransform

--- LP.Plot.Core/Skia/SKTransform.cs
+++ LP.Plot.Core/Skia/SKTransform.cs
@@ -4,19 +4,19 @@ namespace LP.Plot.Core.Skia;
 
 public class SKSpaceTransform
 {
-    LinearTransform xTransform;
-    LinearTransform yTransform;
+    LPTransform xTransform;
+    LPTransform yTransform;
 
     public SKSpaceTransform(LPSize imageSize, Span xRange, Span yRange) : this(imageSize.Width, imageSize.Height, xRange, yRange) { }
 
     public SKSpaceTransform(int imageWidth, int imageHeight, Span xRange, Span yRange)
     {
-        this.xTransform = new LinearTransform(xRange, imageWidth);
-        this.yTransform = new LinearTransform(yRange.Max, yRange.Min, imageHeight);// swap min/max because 0 is top in skia canvas
+        this.xTransform = new LPTransform(xRange, 0, imageWidth);
+        this.yTransform = new LPTransform(yRange.Max, yRange.Min, 0, imageHeight);// swap min/max because 0 is top in skia canvas
     }
 
-    public float ToPixelSpaceX(double x) => xTransform.ToPixelSpace(x);
-    public float ToPixelSpaceY(double y) => yTransform.ToPixelSpace(y);
-    public double ToDataSpaceX(double x) => xTransform.ToDataSpace(x);
-    public double ToDataSpaceY(double y) => yTransform.ToDataSpace(y);
+    public float ToPixelSpaceX(double x) => (float)xTransform.Transform(x);
+    public float ToPixelSpaceY(double y) => (float)yTransform.Transform(y);
+    public double ToDataSpaceX(double x) => xTransform.Inverse(x);
+    public double ToDataSpaceY(double y) => yTransform.Inverse(y);
 }
