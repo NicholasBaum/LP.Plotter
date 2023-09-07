695f59d6de538e16758c9cdb1209394f1ff1f113
NB <n.baumgaertner@hotmail.com>
2023-09-07 17:23:41 +0200

    made readonly

--- LP.Plotter/Data/PlotVM.cs
+++ LP.Plotter/Data/PlotVM.cs
@@ -6,18 +6,18 @@ namespace LP.Plotter.Data;
 public class PlotVM
 {
     public readonly Plot.Core.Plot Plot;
+    public IReadOnlyList<SignalSet> Sets => sets;
+    private List<SignalSet> sets = new List<SignalSet>();
 
     public PlotVM(IEnumerable<ISignal> signals, string xAxisTitle)
     {
         this.Plot = new(signals, xAxisTitle);
-        this.Sets.Add(new SignalSet(signals, "Default Set"));
+        this.sets.Add(new SignalSet(signals, "Default Set"));
     }
-
-    public List<SignalSet> Sets { get; set; } = new();
 
     public void Remove(SignalSet set)
     {
-        Sets.Remove(set);
+        sets.Remove(set);
         foreach (var s in set.Channels)
             Plot.Remove(s.Source);
         Plot.Invalidate();
