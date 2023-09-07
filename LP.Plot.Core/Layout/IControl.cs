0e59a464bb4dc248271d8bcafcb2bc47f4fb109d
NB <n.baumgaertner@hotmail.com>
2023-09-07 02:02:19 +0200

    refactor

--- LP.Plot.Core/Layout/IControl.cs
+++ LP.Plot.Core/Layout/IControl.cs
@@ -1,5 +1,4 @@
 ﻿using LP.Plot.Core.Primitives;
-using SkiaSharp;
 
 namespace LP.Plot.Core.Layout;
 
@@ -9,27 +8,4 @@ public interface IControl : IRenderable
     public LPSize DesiredSize { get; }
     public LPRect Rect { get; }
     public void SetRect(LPRect rect);
-
-    public IDisposable TransformedCanvas(SKCanvas canvas)
-    {
-        canvas.Save();
-        canvas.ClipRect(Rect.ToSkia());
-        canvas.Translate(Rect.Left, Rect.Top);
-        return new TransformedCanvasObj(canvas);
-    }
-
-    internal class TransformedCanvasObj : IDisposable
-    {
-        public readonly SKCanvas Canvas;
-
-        public TransformedCanvasObj(SKCanvas canvas)
-        {
-            this.Canvas = canvas;
-        }
-
-        public void Dispose()
-        {
-            Canvas.Restore();
-        }
-    }
 }
---
+++ LP.Plot.Core/Layout/IControlExtension.cs
@@ -0,0 +1,29 @@
+﻿using SkiaSharp;
+
+namespace LP.Plot.Core.Layout;
+
+public static class IControlExtension
+{
+    public static IDisposable TransformedCanvas(this IControl control, SKCanvas canvas)
+    {
+        canvas.Save();
+        canvas.ClipRect(control.Rect.ToSkia());
+        canvas.Translate(control.Rect.Left, control.Rect.Top);
+        return new TransformedCanvasObj(canvas);
+    }
+
+    internal class TransformedCanvasObj : IDisposable
+    {
+        public readonly SKCanvas Canvas;
+
+        public TransformedCanvasObj(SKCanvas canvas)
+        {
+            this.Canvas = canvas;
+        }
+
+        public void Dispose()
+        {
+            Canvas.Restore();
+        }
+    }
+}
