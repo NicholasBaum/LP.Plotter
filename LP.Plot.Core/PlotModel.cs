bddba4182a685ec03548e0d4538b7baf249a1614
NB <n.baumgaertner@hotmail.com>
2023-09-10 15:11:26 +0200

    axis refreshing

--- LP.Plot.Core/PlotModel.cs
+++ LP.Plot.Core/PlotModel.cs
@@ -16,6 +16,9 @@ public partial class PlotModel : IRenderable
     private int bottomAxisHeight = 75;
     private LPSize canvasSize;
     private RenderInfo renderInfo = new();
+    private AxisControl leftAxisControl;
+    private AxisControl rightAxisControl;
+
 
     public PlotModel() : this(new ISignal[0], "") { }
     public PlotModel(string xAxisTitle) : this(new ISignal[0], xAxisTitle) { }
@@ -24,65 +27,12 @@ public partial class PlotModel : IRenderable
     {
         signalPlot = new BufferedSignalPlot(signals);
         signalPlot.XAxis.Title = xAxisTitle ?? "";
-        SetDefaultYAxes();
         layout.Bottom = new AxisControl(signalPlot.XAxis, this) { Parent = layout, DesiredSize = new LPSize(0, bottomAxisHeight) };
         layout.Center = new SignalPlotControl(signalPlot, this) { Parent = layout };
         layout.Top = BorderControl.CreateBottom(layout, topCellHeight);
-    }
 
-    public void SetDefaultYAxes()
-    {
-        var leftAxis = signalPlot.YAxes.Where(x => x.Position == AxisPosition.Left).FirstOrDefault();
-        var rightAxis = signalPlot.YAxes.Where(x => x.Position == AxisPosition.Right).FirstOrDefault();
-
-        if (leftAxis is not null)
-            layout.Left = new AxisControl(leftAxis, this) { Parent = layout, DesiredSize = new LPSize(leftAxisWidth, 0) };
-        else
-            layout.Left = BorderControl.CreateRight(layout, leftAxisWidth);
-
-        if (rightAxis is not null)
-            layout.Right = new AxisControl(rightAxis, this) { Parent = layout, DesiredSize = new LPSize(rightAxisWidth, 0) };
-        else
-            layout.Right = BorderControl.CreateLeft(layout, rightAxisWidth);
-    }
-
-    public void Render(IRenderContext ctx)
-    {
-        using (var m = renderInfo.Measure())
-        {
-            canvasSize = ctx.Size;
-            ctx.Canvas.Clear(SKColors.Black);
-            layout.SetRect(LPRect.Create(ctx.Size));
-            layout.Render(ctx);
-            DrawZoomRect(ctx);
-        }
-        renderInfo.Render(ctx);
-    }
-
-    public void SetLeftAxis(Axis? axis)
-    {
-        if (axis is null)
-        {
-            layout.Left = BorderControl.CreateRight(layout, leftAxisWidth);
-            return;
-        }
-        if ((this.layout.Right as AxisControl)?.Content == axis)
-            throw new InvalidOperationException("Axis object is already used on the right side.");
-        axis.Position = AxisPosition.Left;
-        this.layout.Left = new AxisControl(axis, this) { Parent = layout, DesiredSize = new LPSize(leftAxisWidth, 0) };
-    }
-
-    public void SetRightAxis(Axis? axis)
-    {
-        if (axis is null)
-        {
-            layout.Right = BorderControl.CreateLeft(layout, rightAxisWidth);
-            return;
-        }
-        if ((this.layout.Left as AxisControl)?.Content == axis)
-            throw new InvalidOperationException("Axis object is already used on the left side.");
-        axis.Position = AxisPosition.Right;
-        this.layout.Right = new AxisControl(axis, this) { Parent = layout, DesiredSize = new LPSize(rightAxisWidth, 0) };
+        leftAxisControl = new AxisControl(this) { Parent = layout, DesiredSize = new LPSize(leftAxisWidth, 0) };
+        rightAxisControl = new AxisControl(this) { Parent = layout, DesiredSize = new LPSize(rightAxisWidth, 0) };
     }
 
     public void Add(ISignal signal)
@@ -99,17 +49,45 @@ public partial class PlotModel : IRenderable
     public void Remove(ISignal signal)
     {
         signalPlot.Remove(signal);
-        RemoveUnassigendAxisControls();
     }
 
-    private void RemoveUnassigendAxisControls()
+    public void Render(IRenderContext ctx)
     {
-        var axes = signalPlot.YAxes;
-        if (layout.Left is AxisControl lac && !axes.Contains(lac.Content))
-            SetLeftAxis(null);
-        if (layout.Right is AxisControl rac && !axes.Contains(rac.Content))
-            SetRightAxis(null);
+        UpdateAxesPosition();
+
+        using (var m = renderInfo.Measure())
+        {
+            canvasSize = ctx.Size;
+            ctx.Canvas.Clear(SKColors.Black);
+            layout.SetRect(LPRect.Create(ctx.Size));
+            layout.Render(ctx);
+            DrawZoomRect(ctx);
+        }
+        renderInfo.Render(ctx);
     }
+
+    private void UpdateAxesPosition()
+    {
+        // only show axes that are attached to a signal
+        var leftAxis = signalPlot.YAxes.Where(x => x.Position == AxisPosition.Left).FirstOrDefault();
+        var rightAxis = signalPlot.YAxes.Where(x => x.Position == AxisPosition.Right).FirstOrDefault();
+
+        if (leftAxis is not null)
+        {
+            leftAxisControl.Content = leftAxis;
+            layout.Left = leftAxisControl;
+        }
+        else
+            layout.Left = BorderControl.CreateRight(layout, leftAxisWidth);
+
+        if (rightAxis is not null)
+        {
+            rightAxisControl.Content = rightAxis;
+            layout.Right = rightAxisControl;
+        }
+        else
+            layout.Right = BorderControl.CreateLeft(layout, rightAxisWidth);
+    }  
 
     public void ResetAxes()
     {
